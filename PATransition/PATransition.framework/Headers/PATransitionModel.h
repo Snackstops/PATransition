7b7e040a1ff8452d9faafa3673ed91d83bbdacec
apple <snackstops@MacBook-Pro-13.local>
2020-07-17 14:35:01 +0800

    '提交内容'

--- PATransition/PATransition.framework/Headers/PATransitionModel.h
+++ PATransition/PATransition.framework/Headers/PATransitionModel.h
@@ -11,13 +11,41 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PATransitionModel : NSObject
-
+/**
+ BOUNDID
+ 默认空
+ */
 @property (nonatomic, strong) NSString *BOUNDID;
-@property (nonatomic, strong) NSString *TIME;//20200101120000
+/**
+ 时间
+ 20200101120000
+ */
+@property (nonatomic, strong) NSString *TIME;
+/**
+背景颜色
+默认白色
+*/
 @property (nonatomic, strong) UIColor *BGColor;
-//@property (nonatomic, strong) UIImage *BGIMG;
-@property (nonatomic) BOOL ContainsNavigation;//是否包含导航栏
-@property (nonatomic) BOOL ContainsTabBar;//是否包含TaB
+/**
+背景图片
+默认空
+*/
+@property (nonatomic, strong) UIImage *BGIMG;
+/**
+是否显示Loading
+默认YES
+*/
+@property (nonatomic) BOOL SHOWLOADING;
+/**
+是否包含导航栏
+默认NO
+*/
+@property (nonatomic) BOOL ContainsNavigation;
+/**
+是否包含TaB
+默认NO
+*/
+@property (nonatomic) BOOL ContainsTabBar;
 
 
 @end

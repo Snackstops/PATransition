c58ccc10dd2a9393bb27274f3a0ba264551ac05b
apple <snackstops@MacBook-Pro-13.local>
2020-07-17 11:11:35 +0800

    '提交内容'

--- PATransition/PATransition.framework/Headers/PAT_IMGZOOMViewController.h
+++ PATransition/PATransition.framework/Headers/PAT_IMGZOOMViewController.h
@@ -11,7 +11,25 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PAT_IMGZOOMViewController : UIViewController
-@property (nonatomic,strong) UIImageView *imageView;
+@property (nonatomic,strong) UIColor *BGColor;//背景颜色 默认黑
+@property (nonatomic,strong) UIImageView *imageView;//显示图片
+@property (nonatomic) CGRect ImgFrame;//设置位置大小 默认70%
+@property (nonatomic) BOOL ImgCenter;//是否居中 默认居中
+
+/**
+ UIImageView *imageView  = (UIImageView *)recognizer.view;
+ PAT_IMGZOOMViewController *vc = [[PAT_IMGZOOMViewController alloc] init];
+ vc.imageView.image  = imageView.image;
+ __weak PAT_IMGZOOMViewController *weakVC = vc;
+ [self.navigationController PA_pushViewController:vc makeTransition:^(PATransitionProperty *transition) {
+     transition.animationType = PATransitionAnimationTypeViewMoveToNextVC;
+     //transition.animationTime = 0.64;
+     transition.startView  = imageView;
+     transition.targetView = weakVC.imageView;
+ }];
+ */
+
+
 @end
 
 NS_ASSUME_NONNULL_END

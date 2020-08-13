e4c3950db81be9a6ecd01cb98579b786e6f1b0f7
apple <snackstops@MacBook-Pro-13.local>
2020-08-13 15:01:41 +0800

    '提交内容'

--- PATransition/PATransition.framework/Headers/PATransition.h
+++ PATransition/PATransition.framework/Headers/PATransition.h
@@ -17,11 +17,9 @@ FOUNDATION_EXPORT const unsigned char PATransitionVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <PATransition/PublicHeader.h>
 
 
-#import <PATransition/PATransitionTools.h>
+
 #import <PATransition/PATransitionModel.h>
-
 #import <PATransition/UINavigationController+PATransition.h>
-
 #import <PATransition/PATransitionManager+BoomAnimation.h>
 #import <PATransition/PATransitionManager+ViewMoveAnimation.h>
 #import <PATransition/PATransitionManager+CoverAnimation.h>
@@ -34,21 +32,13 @@ FOUNDATION_EXPORT const unsigned char PATransitionVersionString[];
 #import <PATransition/UIViewController+PATransition.h>
 #import <PATransition/PATransitionManager+FragmentAnimation.h>
 #import <PATransition/PATransitionManager+PageAnimation.h>
-
 #import <PATransition/PAT_IMGZOOMViewController.h>
-
-//#import <PATransition/PATransitionNetworkingTools.h>
-//#import <PATransition/PushEncapsulationTools.h>
-//#import <PATransition/PushEncapsulation.h>
-//#import <PATransition/PATransitionToolsVC.h>
-//#import <PATransition/PATransitionToolsView.h>
-//#import <PATransition/PATransitionServerVC.h>
-//#import "AppDelegate+PushEncapsulation.h>
 #import <PATransition/PATransitionProperty.h>
 #import <PATransition/PATransitionManager.h>
 #import <PATransition/PATransitionManager+InsideThenPushAnimation.h>
 #import <PATransition/PATransitionManager+SpreadAnimation.h>
 #import <PATransition/PATransitionManager+SystermAnimation.h>
 
+#import <PATransition/PATransitionTools.h>
 
 
